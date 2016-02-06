12548e356866fde44955c968d88a0f6a6bf48df1
c41x <jakubduracz@gmail.com>
2016-02-06 12:20:22 +0100

    + process-text-file.rkt

---
+++ process-text-file.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(define (preprocess in)
+  (let ((line (read-line in 'any)))
+    (unless (eof-object? line)
+      (unless (regexp-match #rx";;;;[^;]*?$" line)
+        (display line out)
+        (newline out))
+      (preprocess in))))
+
+(define out (open-output-file "data.processed.raw.txt" #:exists 'truncate))
+(call-with-input-file "data.raw.txt" preprocess)
+(close-output-port out)
