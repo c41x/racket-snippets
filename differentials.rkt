adbb4b1b21d39aaf09f86c871a1198583c628ca8
c41x <jakubduracz@gmail.com>
2016-03-04 16:56:51 +0100

    + simple version

--- differentials.rkt
+++ differentials.rkt
@@ -62,4 +62,17 @@
 			(make-product-nice (deriv (p1 exp) var)
 					   (p2 exp))))))
 
+;; without simplification
+(define (deriv-simple exp var)
+  (cond ((constant? exp var) 0) ;; derivative of const is 0
+	((same-var? exp var) 1) ;; derivative of variable is 1
+	((sum? exp) ;; derivative of sum is sum of derivatives of expressions
+	 (make-sum (deriv (s1 exp) var)
+		   (deriv (s2 exp) var)))
+	((product? exp) ;; (f*g)' = f'*g + f*g'
+	 (make-sum (make-product (p1 exp)
+				 (deriv (p2 exp) var))
+		   (make-product (deriv (p1 exp) var)
+				 (p2 exp))))))
+
 (deriv '(* x x) 'x)
